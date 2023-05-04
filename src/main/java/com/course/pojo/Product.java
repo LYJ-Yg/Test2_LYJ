be38a8a6fda6f003dc343549a925636390632880
lyj <2724042454@qq.com>
2023-05-04 15:08:27 +0800

    产品

---
+++ src/main/java/com/course/pojo/Product.java
@@ -0,0 +1,52 @@
+package com.course.pojo;
+
+import java.util.List;
+
+public class Product {
+    private int id;
+    private String name;
+    private  double price;
+    private List<Orders> ordersList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<Orders> getOrdersList() {
+        return ordersList;
+    }
+
+    public void setOrdersList(List<Orders> ordersList) {
+        this.ordersList = ordersList;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", ordersList=" + ordersList +
+                '}';
+    }
+}
